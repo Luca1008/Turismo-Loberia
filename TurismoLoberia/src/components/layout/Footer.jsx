b4e30a92a224c9bb772c02009500a870f8b29f8c
Luca1008 <lguidi@alumnos.exa.unicen.edu.ar>
2025-07-02 10:55:00 -0300

    v1 Footer

--- TurismoLoberia/src/components/layout/Footer.jsx
+++ TurismoLoberia/src/components/layout/Footer.jsx
@@ -1,35 +1,54 @@
 import React from 'react'
 import '../../styles/footer.css'; // Asegúrate de tener este archivo
+import logoLoberia from "../../assets/icons/logoLoberia.svg";
+import { TiSocialFacebookCircular } from "react-icons/ti";
+import { TiSocialInstagram } from "react-icons/ti";
+import { IoLogoYoutube } from "react-icons/io5";
+import { FaWhatsapp } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+
 
 export const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__logo">
-        <img src="/assets/logo-loberia.png" alt="Logo Lobería" />
-        <p>GOBIERNO LOCAL</p>
+          <img
+            className="logoLoberia"
+            src={logoLoberia}
+            alt="Lobería"
+            width="40"
+            height="40"
+          />
       </div>
-
       <div className="footer__info">
-        <div className="footer__item">
-          <span>📧</span>
-          <p>Contactate con nosotros</p>
-        </div>
-        <div className="footer__item">
-          <span>📍</span>
-          <p>Dirección de Turismo, Av. Campos 500</p>
-        </div>
-        <div className="footer__item">
-          <span>📢</span>
-          <p>Suscribite a los newsletter</p>
-        </div>
-      </div>
+            <div className="footer__item">
+              <span>📧</span>
+              <p>Contactate con nosotros</p>
+            </div>
 
-      <div className="footer__social">
-        <a href="#"><i className="fab fa-facebook"></i></a>
-        <a href="#"><i className="fab fa-instagram"></i></a>
-        <a href="#"><i className="fab fa-youtube"></i></a>
-        <a href="#"><i className="fab fa-whatsapp"></i></a>
-      </div>
+            <div className="footer__item">
+              <span>📍</span>
+              <p>Dirección de Turismo, Av. Campos 500</p>
+            </div>
+        </div>
+        <div className="footer__info">
+            <div className="footer__item">
+              <span>📢</span>
+              <p>Suscribite a los newsletter</p>
+            </div>
+
+            <div className="footer__social">
+              <a href="#"><TiSocialFacebookCircular /></a>
+              <a href="#"><TiSocialInstagram /></a>
+              <a href="#"><IoLogoYoutube /></a>
+              <a href="#"><FaWhatsapp /></a>
+              <a href=""><FaTiktok /></a>
+            </div>
+        </div>
     </footer>
   );
 };
