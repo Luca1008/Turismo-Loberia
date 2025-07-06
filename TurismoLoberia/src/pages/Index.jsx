68fb3714b5bf19ea60a3199344a6bedc86085965
Felicitas <felicitas.aguerralde@gmail.com>
2025-07-06 11:55:34 -0300

    Agrego section services

--- TurismoLoberia/src/pages/Index.jsx
+++ TurismoLoberia/src/pages/Index.jsx
@@ -3,11 +3,48 @@ import { Footer } from "../components/layout/Footer";
 import { ControlledCarousel } from "../components/layout/ControlledCarousel";
 import React from "react";
 
+import {
+  MdHotel,
+  MdOutlineRestaurant,
+  MdPlace,
+  MdMuseum,
+} from "react-icons/md";
+import { FaPaintBrush, FaInfo } from "react-icons/fa";
+
 export const Index = () => {
   return (
     <div className="index">
       <Header />
       <ControlledCarousel />
+      <section className="services">
+        <h2>Guía de Servicios</h2>
+        <div className="item-services-container">
+          <div className="item-services">
+            <MdHotel />
+            <p>Alojamientos</p>
+          </div>
+          <div className="item-services">
+            <MdOutlineRestaurant />
+            <p>Gastronomía</p>
+          </div>
+          <div className="item-services">
+            <MdPlace />
+            <p>Lugares Interés</p>
+          </div>
+          <div className="item-services">
+            <FaPaintBrush />
+            <p>Artesanos</p>
+          </div>
+          <div className="item-services">
+            <MdMuseum />
+            <p>Servicios Públicos</p>
+          </div>
+          <div className="item-services">
+            <FaInfo />
+            <p>Info Útil</p>
+          </div>
+        </div>
+      </section>
       <Footer />
     </div>
   );
