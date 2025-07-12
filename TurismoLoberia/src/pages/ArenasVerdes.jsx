c0e2fbb00acfc98b25c7c0b9e408e1f64255aa54
Felicitas <felicitas.aguerralde@gmail.com>
2025-07-12 10:02:13 -0300

    Creo pagina ciudades

---
+++ TurismoLoberia/src/pages/ArenasVerdes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ArenasVerdes = () => {
+  return (
+    <div>ArenasVerdes</div>
+  )
+}
+
+export default ArenasVerdes
---
+++ TurismoLoberia/src/pages/Loberia.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loberia = () => {
+  return (
+    <div>Loberia</div>
+  )
+}
+
+export default Loberia
