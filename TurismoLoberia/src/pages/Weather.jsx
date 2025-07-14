fc71f27bc4786908ef33e01a2a08a86b40dfa8ba
Felicitas <felicitas.aguerralde@gmail.com>
2025-07-14 09:38:58 -0300

    Modo oscuro, saque margenes y eliminé archivos q no se usan

--- TurismoLoberia/src/pages/Weather.jsx
+++ TurismoLoberia/src/pages/Weather.jsx
@@ -1,23 +1,15 @@
 import React from "react";
 import WeatherCard from "../components/cards/WeatherCard";
-import WeatherCarousel from "../components/layout/WeatherCarousel";
+import '../styles/weather.css';
 
 export const Clima = () => {
   return (
     <div>
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          gap: 32,
-          justifyContent: "center",
-          marginTop: 40,
-        }}
-      >
+      <section className="section-weather">
         <WeatherCard ciudad="Lobería" />
         <WeatherCard ciudad="San Manuel" />
         <WeatherCard ciudad="Arenas Verdes" lat={-38.8083} lon={-58.6036} />
-      </div>
+      </section>
     </div>
   );
 };
