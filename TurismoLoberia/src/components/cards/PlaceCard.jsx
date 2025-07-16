10582728cb46344f7233fb159eb99063739815a6
Luca1008 <lguidi@alumnos.exa.unicen.edu.ar>
2025-07-16 11:36:50 -0300

    Modifico place card

--- TurismoLoberia/src/components/cards/PlaceCard.jsx
+++ TurismoLoberia/src/components/cards/PlaceCard.jsx
@@ -1,7 +1,13 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import placeCard1 from "../../assets/images/place-card-index/place-card-1.jpg";
+
 const PlaceCard = ({ title, description, city, img }) => {
+  // Default values for props
+  title = title || "Título del lugar";
+  description = description || "Descripción breve del lugar.";
+  city = city || "Ciudad no especificada";
+  img = img || placeCard1;
   return (
     <Card className="place-card">
       <Card.Img variant="top" src={img || "https://via.placeholder.com/300x200?text=Sin+Imagen"}
