1c703f9c434721d079ab436da718ac2514a1f20a
Felicitas <felicitas.aguerralde@gmail.com>
2025-07-20 16:12:42 -0300

    Elimino archivos innecesarios

--- Backend/middlewares/upload.js
+++
@@ -1,7 +0,0 @@
-// ✅ Almacenamiento en memoria (para guardar imagen como BYTEA en PostgreSQL)
-const multer = require('multer');
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
-module.exports = upload;
