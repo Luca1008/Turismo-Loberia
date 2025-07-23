312c2a535938eebd1e8066680dd617f948a13be1
Luca1008 <lguidi@alumnos.exa.unicen.edu.ar>
2025-07-22 23:15:42 -0300

    ROmpi todo

--- TurismoLoberia/src/main.jsx
+++ TurismoLoberia/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import './styles/style.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./i18n/i18n";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
